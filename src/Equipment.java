bbce23b5cf1566a95560b680b0adcc5dd1f7e6bb
Equinox- <equinoxscripts@gmail.com>
2011-05-04 12:11:01 -0700

    Fixed the scary orginization by dividing up the classes & enums.  Also got rid of the Location class..  Really now.  new Location(new RSTile(5,5),0) == new RSTile(5,5,0)
    Tiles have a Z variable aka a PLANE variable.
    
    Change-Id: Iebc90ec759fff2ea24d6b1d2a3871ebcea210f7e

---
+++ src/Equipment.java
@@ -0,0 +1,21 @@
+class Equipment {
+	private String[] names;
+	private int slot;
+
+	public Equipment(String[] names, int slot) {
+		this.names = names;
+		this.slot = slot;
+	}
+
+	public Equipment(String name, int slot) {
+		this(new String[] { name }, slot);
+	}
+
+	public String[] getNames() {
+		return this.names;
+	}
+
+	public int getSlot() {
+		return this.slot;
+	}
+}
