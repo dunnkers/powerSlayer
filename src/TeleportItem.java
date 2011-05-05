8228f10247bde59fdb012dcc8365c05933702b62
Equinox- <equinoxscripts@gmail.com>
2011-05-04 17:34:38 -0700

    More tele items, more tele-spells, and a slayer master tile.
    Change-Id: I4e76d897aac84d203d7709557bc963a530e7ed52

--- src/TeleportItem.java
+++ src/TeleportItem.java
@@ -1,49 +1,61 @@
 import org.rsbot.script.wrappers.RSTile;
 
 public enum TeleportItem implements Teleport {
-    ;
-    private final int[] itemIDs;
-    private final RSTile dest;
-    private final String action;
-    private final int rubInterfaceID;
+	ECTOPHIAL(new RSTile(3658, 3522), "Empty", 4251),
+	LUMBRIDGE_TAB(new RSTile(3221, 3220), 8008),
+	ARDOUGENE_TAB(new RSTile(2661, 3303), 8011),
+	CAMELOT_TAB(new RSTile(2757, 3478), 8010),
+	VARROCK_TAB(new RSTile(3212, 3429), 8007),
+	TAVERLEY_TAB(new RSTile(2893, 3465), 18110),
+	RIMMINGTON_TAB(new RSTile(2956, 3224), 18809),
+	RELLEKA_TAB(new RSTile(2670, 3652), 18812),
+	BRIMHAVEN_TAB(new RSTile(2759, 3179), 18813),
+	NARDAH_SCROLL(new RSTile(3421, 2918), 19475),
+	LUMBER_YARD_SCROLL(new RSTile(3308, 3492), 19480),
+	MISCELLANIA_SCROLL(new RSTile(2513, 3858), 19477),
+	LLETYA_CRYSTAL(new RSTile(2328, 3172), 6102, 6100, 6101);
+	private final int[] itemIDs;
+	private final RSTile dest;
+	private final String action;
+	private final int rubInterfaceID;
 
-    private TeleportItem(RSTile dest, int... itemIDs) {
-	this.dest = dest;
-	this.itemIDs = itemIDs;
-	this.action = "Break";
-	this.rubInterfaceID = -1;
-    }
+	private TeleportItem(RSTile dest, int... itemIDs) {
+		this.dest = dest;
+		this.itemIDs = itemIDs;
+		this.action = "Break";
+		this.rubInterfaceID = -1;
+	}
 
-    private TeleportItem(RSTile dest, String action, int... itemIDs) {
-	this.dest = dest;
-	this.itemIDs = itemIDs;
-	this.action = action;
-	this.rubInterfaceID = -1;
-    }
+	private TeleportItem(RSTile dest, String action, int... itemIDs) {
+		this.dest = dest;
+		this.itemIDs = itemIDs;
+		this.action = action;
+		this.rubInterfaceID = -1;
+	}
 
-    private TeleportItem(RSTile dest, String action, int rubInterfaceID,
-	    int... itemIDs) {
-	this.dest = dest;
-	this.itemIDs = itemIDs;
-	this.action = action;
-	this.rubInterfaceID = rubInterfaceID;
-    }
+	private TeleportItem(RSTile dest, int rubInterfaceID, String action,
+			int... itemIDs) {
+		this.dest = dest;
+		this.itemIDs = itemIDs;
+		this.action = action;
+		this.rubInterfaceID = rubInterfaceID;
+	}
 
-    public int getRubInterfaceID() {
-	return rubInterfaceID;
-    }
+	public int getRubInterfaceID() {
+		return rubInterfaceID;
+	}
 
-    public String getAction() {
-	return action;
-    }
+	public String getAction() {
+		return action;
+	}
 
-    public int[] getIDs() {
-	return itemIDs;
-    }
+	public int[] getIDs() {
+		return itemIDs;
+	}
 
-    @Override
-    public RSTile getDest() {
-	return dest;
-    }
+	@Override
+	public RSTile getDest() {
+		return dest;
+	}
 
 }
--- src/TeleportSpell.java
+++ src/TeleportSpell.java
@@ -5,77 +5,75 @@ import java.util.HashMap;
 import java.util.Map;
 
 enum TeleportSpell implements Teleport {
-    LUMBRIDGE_HOME(Magic.SPELL_HOME_TELEPORT, new RSTile(0, 0, 0), 0), VARROCK(
-	    Magic.SPELL_VARROCK_TELEPORT, new RSTile(0, 0, 0), 25, Rune.FIRE,
-	    3, Rune.AIR, Rune.LAW), MOBILISING_ARMIES(
-	    Magic.SPELL_MOBILISING_ARMIES_TELEPORT, new RSTile(0, 0, 0), 10,
-	    Rune.LAW, Rune.WATER, Rune.AIR), LUMBRIDGE(
-	    Magic.SPELL_LUMBRIDGE_TELEPORT, new RSTile(0, 0, 0), 31, Rune.LAW,
-	    Rune.AIR, 3, Rune.EARTH), FALADOR(Magic.SPELL_FALADOR_TELEPORT,
-	    new RSTile(0, 0), 37, Rune.LAW, Rune.AIR, 3, Rune.WATER), CAMELOT(
-	    Magic.SPELL_CAMELOT_TELEPORT, new RSTile(0, 0), 45, Rune.LAW,
-	    Rune.AIR, 5);
-    private RSTile loc;
-    private int magicLevel;
-    private Map<Rune, Integer> runes = new HashMap<Rune, Integer>();
-    private int spellNum;
-    private Magic.Book book;
+	LUMBRIDGE_HOME(Magic.SPELL_HOME_TELEPORT, new RSTile(3221, 3220), 0),
+	VARROCK(Magic.SPELL_VARROCK_TELEPORT, new RSTile(3212, 3429), 25, Rune.FIRE, 3, Rune.AIR, Rune.LAW),
+	MOBILISING_ARMIES(Magic.SPELL_MOBILISING_ARMIES_TELEPORT, new RSTile(0, 0,
+			0), 10, Rune.LAW, Rune.WATER, Rune.AIR),
+	LUMBRIDGE(Magic.SPELL_LUMBRIDGE_TELEPORT, new RSTile(3221, 3220), 31, Rune.LAW, Rune.AIR, 3, Rune.EARTH),
+	FALADOR(Magic.SPELL_FALADOR_TELEPORT, new RSTile(0, 0), 37, Rune.LAW, Rune.AIR, 3, Rune.WATER),
+	CAMELOT(Magic.SPELL_CAMELOT_TELEPORT, new RSTile(2757, 3478), 45, Rune.LAW, Rune.AIR, 5),
+	ARDOUGENE(Magic.SPELL_ARDOUGNE_TELEPORT, new RSTile(2661, 3303), 51, Rune.LAW, 2, Rune.WATER, 2);
+	private RSTile loc;
+	private int magicLevel;
+	private Map<Rune, Integer> runes = new HashMap<Rune, Integer>();
+	private int spellNum;
+	private Magic.Book book;
 
-    private TeleportSpell(int spellNum, RSTile loc, int magicLevel,
-	    Rune... runes) {
-	this.loc = loc;
-	this.book = Magic.Book.MODERN;
-	this.spellNum = spellNum;
-	this.magicLevel = magicLevel;
-	for (Rune r : runes) {
-	    Integer oldVal = this.runes.get(r);
-	    this.runes.put(r, (oldVal != null ? oldVal.intValue() : 0) + 1);
-	}
-    }
-
-    private TeleportSpell(int spellNum, RSTile loc, int magicLevel,
-	    Object... runes) {
-	this.loc = loc;
-	this.book = Magic.Book.MODERN;
-	this.spellNum = spellNum;
-	this.magicLevel = magicLevel;
-	for (int i = 0; i < runes.length; i++) {
-	    if (runes[i] instanceof Rune) {
-		Rune rune = (Rune) runes[i];
-		int count = 1;
-		if (i + 1 < runes.length && runes[i + 1] instanceof Integer) {
-		    count = ((Integer) runes[i + 1]).intValue();
+	private TeleportSpell(int spellNum, RSTile loc, int magicLevel,
+			Rune... runes) {
+		this.loc = loc;
+		this.book = Magic.Book.MODERN;
+		this.spellNum = spellNum;
+		this.magicLevel = magicLevel;
+		for (Rune r : runes) {
+			Integer oldVal = this.runes.get(r);
+			this.runes.put(r, (oldVal != null ? oldVal.intValue() : 0) + 1);
 		}
-		Integer old = this.runes.get(rune);
-		this.runes
-			.put(rune, (old != null ? old.intValue() : 0) + count);
-	    }
 	}
-    }
 
-    @Override
-    public RSTile getDest() {
-	return loc;
-    }
+	private TeleportSpell(int spellNum, RSTile loc, int magicLevel,
+			Object... runes) {
+		this.loc = loc;
+		this.book = Magic.Book.MODERN;
+		this.spellNum = spellNum;
+		this.magicLevel = magicLevel;
+		for (int i = 0; i < runes.length; i++) {
+			if (runes[i] instanceof Rune) {
+				Rune rune = (Rune) runes[i];
+				int count = 1;
+				if (i + 1 < runes.length && runes[i + 1] instanceof Integer) {
+					count = ((Integer) runes[i + 1]).intValue();
+				}
+				Integer old = this.runes.get(rune);
+				this.runes
+						.put(rune, (old != null ? old.intValue() : 0) + count);
+			}
+		}
+	}
 
-    public int getMagicLevel() {
-	return magicLevel;
-    }
+	@Override
+	public RSTile getDest() {
+		return loc;
+	}
 
-    public int getSpell() {
-	return spellNum;
-    }
+	public int getMagicLevel() {
+		return magicLevel;
+	}
 
-    public Magic.Book getBook() {
-	return book;
-    }
+	public int getSpell() {
+		return spellNum;
+	}
 
-    public Map<Rune, Integer> getRunes() {
-	return runes;
-    }
+	public Magic.Book getBook() {
+		return book;
+	}
 
-    public int getRuneCount(Rune rune) {
-	Integer i = runes.get(rune);
-	return i != null ? i.intValue() : 0;
-    }
+	public Map<Rune, Integer> getRunes() {
+		return runes;
+	}
+
+	public int getRuneCount(Rune rune) {
+		Integer i = runes.get(rune);
+		return i != null ? i.intValue() : 0;
+	}
 }
