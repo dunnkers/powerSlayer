829ad7ca4c5afcff55fec4e5554cbe6930d1eb7d
Equinox- <equinoxscripts@gmail.com>
2011-05-06 21:23:59 -0700

    Monster Tiles + More Slayer Masters
    Change-Id: I360376348207675c889453cc004eeb6883705989

--- src/org/powerbot/powerslayer/data/SlayerMaster.java
+++ src/org/powerbot/powerslayer/data/SlayerMaster.java
@@ -3,50 +3,58 @@ package org.powerbot.powerslayer.data;
 import org.rsbot.script.wrappers.RSTile;
 
 public enum SlayerMaster {
-    // TODO Add other slayer masters + locations
-    MAZCHNA("Mazchna", new RSTile(3510, 3509, 0), 20),
-    TURAEL("Turael", new RSTile(0, 0, 0), 3);
-    private RSTile location;
-    private String name;
-    private int combatLevel;
-    private int slayerLevel;
+	// TODO Add other slayer masters + locations
+	MAZCHNA(new String[] { "Mazchna", "Achtryn" }, new RSTile(3510, 3509, 0), 20),
+	TURAEL(new String[] { "Turael", "Spria" }, new RSTile(0, 0, 0), 3),
+	VANNAKA(new String[] { "Vannaka" }, new RSTile(0, 0), 40),
+	CHAELDAR(new String[] { "Chaeldar" }, new RSTile(0, 0), 70),
+	SUMONA(new String[] { "Sumona" }, new RSTile(0, 0), 85, 35),
+	DURADEL(new String[] { "Duradel", "Lapalok" }, new RSTile(0, 0), 100, 50),
+	KURADEL(new String[] { "Kuradel" }, new RSTile(0, 0), 110, 75);
+	private RSTile location;
+	private String[] name;
+	private int combatLevel;
+	private int slayerLevel;
 
-    private SlayerMaster(String name, RSTile location, int combatLevel) {
-        this.name = name;
-        this.location = location;
-        this.combatLevel = combatLevel;
-        this.slayerLevel = 0;
-    }
+	private SlayerMaster(String[] name, RSTile location, int combatLevel) {
+		this.name = name;
+		this.location = location;
+		this.combatLevel = combatLevel;
+		this.slayerLevel = 0;
+	}
 
-    private SlayerMaster(String name, RSTile location, int combatLevel, int slayerLevel) {
-        this.name = name;
-        this.location = location;
-        this.combatLevel = combatLevel;
-        this.slayerLevel = slayerLevel;
-    }
+	private SlayerMaster(String[] name, RSTile location, int combatLevel,
+			int slayerLevel) {
+		this.name = name;
+		this.location = location;
+		this.combatLevel = combatLevel;
+		this.slayerLevel = slayerLevel;
+	}
 
-    public SlayerMaster get(String name) {
-        for (SlayerMaster master : values()) {
-            if (name.toLowerCase().contains(master.getName().toLowerCase())) {
-                return master;
-            }
-        }
-        return null;
-    }
+	public SlayerMaster get(String name) {
+		for (SlayerMaster master : values()) {
+			for (String s : master.getNames()) {
+				if (name.toLowerCase().contains(s.toLowerCase())) {
+					return master;
+				}
+			}
+		}
+		return null;
+	}
 
-    public RSTile getLocation() {
-        return this.location;
-    }
+	public RSTile getLocation() {
+		return this.location;
+	}
 
-    public String getName() {
-        return this.name;
-    }
+	public String[] getNames() {
+		return this.name;
+	}
 
-    private int getSlayerLevel() {
-        return this.slayerLevel;
-    }
+	private int getSlayerLevel() {
+		return this.slayerLevel;
+	}
 
-    private int getCombatLevel() {
-        return this.combatLevel;
-    }
+	private int getCombatLevel() {
+		return this.combatLevel;
+	}
 }
