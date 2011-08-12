2bdcc699d1bfce5d8007ef0917337cf553e1f4ff
Wade <logo2462@gmail.com>
2011-08-12 14:38:35 -0600

    rewrote method base to not include renatusbot functions as they are all static now

--- src/org/powerbot/powerslayer/common/MethodBase.java
+++ src/org/powerbot/powerslayer/common/MethodBase.java
@@ -5,7 +5,6 @@ import org.powerbot.powerslayer.methods.Banking;
 import org.powerbot.powerslayer.methods.SlayerMasters;
 import org.powerbot.powerslayer.methods.Traveling;
 import org.powerbot.powerslayer.methods.UniversalFighter;
-import org.rsbot.script.methods.*;
 
 import java.util.Random;
 
@@ -14,44 +13,7 @@ import java.util.Random;
  * org.powerbot.powerslayer.common;
  */
 public class MethodBase {
-	//RSBot classes
     public PowerSlayer parent = null;
-    public Account account = null;
-    public Bank bank = null;
-    public Calculations calculations = null;
-    public Camera camera = null;
-    public Combat combat = null;
-    public ClanChat clanChat = null;
-    public Environment environment = null;
-    public Equipment equipment = null;
-    public FriendChat friendChat = null;
-    public Game game = null;
-    //public GrandExchange grandExchange = null;
-    public GroundItems groundItems = null;
-    //public Hiscores hiscores = null;
-    public Interfaces interfaces = null;
-    public Inventory inventory = null;
-    public Keyboard keyboard = null;
-	public Lobby lobby = null;
-    public Magic magic = null;
-    public Menu menu = null;
-    public Mouse mouse = null;
-    public NPCs npcs = null;
-    public Objects objects = null;
-    public Players players = null;
-    public Prayer prayer = null;
-	public Projectiles projectiles = null;
-    public Settings settings = null;
-	public Quests quests = null;
-    public Skills skills = null;
-   // public Store store = null;
-    //public Summoning summoning = null;
-    public Tiles tiles = null;
-    //public Trade trade = null;
-    public Walking walking = null;
-    //public Web web = null;
-    
-    //Internal classes
     public UniversalFighter fighter = new UniversalFighter(this);
     public Banking bankingProcess = new Banking(this);
     public Traveling travel = new Traveling(this);
