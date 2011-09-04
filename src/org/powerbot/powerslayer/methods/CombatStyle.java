5738f08357d33583d848c5912a47a0965e054a36
Dunnkers Habinio <dunnkers@hotmail.com>
2011-09-04 03:01:54 +0200

    Changed methods.CombatStyle.hasStyle and commented more unneccary code.

--- src/org/powerbot/powerslayer/methods/CombatStyle.java
+++ src/org/powerbot/powerslayer/methods/CombatStyle.java
@@ -1,11 +1,42 @@
 package org.powerbot.powerslayer.methods;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 //TODO: Discuss movement/ rewriting
 public class CombatStyle {
 
-	private final Style[] style;
-	//private final int[] maxHits;
+	private final ArrayList<Style> styles;
+	
+	public CombatStyle(final Style... styles) {
+		this.styles = new ArrayList<Style>(Arrays.asList(styles));
+	}
+	
+	public boolean hasStyle(Style... styles) {
+		if (styles == null || !(styles.length > 0)) {
+			
+		}
+		for (final Style style : styles) {
+			if (!this.styles.contains(style)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public enum Style {
+		MELEE,
+		MAGIC,
+		RANGE,
+		DRAGONFIRE,
+		RANGED_DRAGONFIRE,
+		MAGICAL_MELEE,
+		MAGICAL_RANGE
+	}
+	
+	/*private final Style[] styles;
+	 * private final int[] maxHits;
 
 	public CombatStyle(final Style a, final Style... style) {
 		this.style = new Style[1 + style.length];
@@ -13,8 +44,8 @@ public class CombatStyle {
 		for (int i = 0; i < style.length; i++) {
 			this.style[i + 1] = style[0];
 		}
-		//maxHits = new int[this.style.length];
-		//Arrays.fill(maxHits, -1);
+		maxHits = new int[this.style.length];
+		Arrays.fill(maxHits, -1);
 	}
 
 	public Style getStyle() {
@@ -31,7 +62,7 @@ public class CombatStyle {
 		}
 	}
 
-	/*public int getMaxHit(int tier) {
+	public int getMaxHit(int tier) {
 		if (tier < 0) {
 			return maxHits[0];
 		} else if (tier >= maxHits.length) {
@@ -39,23 +70,19 @@ public class CombatStyle {
 		} else {
 			return maxHits[tier];
 		}
-	}*/
-
-	public boolean hasStyle(Style style) {
-		for (Style s : this.style) {
-			if (s.equals(style)) {
-				return true;
-			}
-		}
-		return false;
 	}
-
+	
 	// TODO: write setStyle method
 	public boolean setStyle(Style style) {
 		return false;
 	}
 
-	public enum Style {
-		MELEE, MAGIC, RANGE, DRAGONFIRE, RANGED_DRAGONFIRE, MAGICAL_MELEE, MAGICAL_RANGE
-	}
+	public boolean hasStyle(Style style) {
+		for (Style s : this.styles) {
+			if (s.equals(style)) {
+				return true;
+			}
+		}
+		return false;
+	}*/
 }
